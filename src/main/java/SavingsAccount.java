aaf70b21d28a8bcb82c053842d32b1d25748f0f7
Revathi G <revathikrishna0323@gmail.com>
2022-08-23 21:30:45 +0530

    Properly formatted and final commit.

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,19 +1,23 @@
 public class SavingsAccount {
-   long accountNumber;
-   String name;
-   double accountBalance;
-   String status;
-   public double debitCash(double cash) {
-      return cash;
-   }
-   public double creditCash(double cash) {
-      return cash;
-   }
-   public double handleTransfers(double history) {
-      return history;
-   }
-   public double checkBalance(double balance) {
-      return balance;
-   }
+    long accountNumber;
+    String name;
+    double accountBalance;
+    String status;
+
+    public double debitCash(double cash) {
+        return cash;
+    }
+
+    public double creditCash(double cash) {
+        return cash;
+    }
+
+    public double handleTransfers(double history) {
+        return history;
+    }
+
+    public double checkBalance(double balance) {
+        return balance;
+    }
 
 }
