d26bb292f28ad9b4015a0f31151f1eb645f904aa
Revathi G <revathikrishna0323@gmail.com>
2022-08-23 21:36:10 +0530

    Properly formatted and final commit

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,12 +2,15 @@ public class Customer {
     public double withdrawCash(double cash) {
         return cash;
     }
+
     public String checkBalance(String cash) {
         return cash;
     }
+
     public double depositCash(double cash) {
         return cash;
     }
+
     public double transferFund(double cash) {
         return cash;
     }
